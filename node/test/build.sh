46a5e83cbd8845a347640095a45e83ddea62bc29
Joseph Lee <joseph@jc-lab.net>
2024-10-10 20:03:12 +0900

    fix

--- node/test/build.sh
+++ node/test/build.sh
@@ -6,7 +6,4 @@ SCRIPT_DIR="$( dirname -- "$BASH_SOURCE"; )";
 
 cd ${SCRIPT_DIR}/../../
 
-export GOROOT=$HOME/go/go1.23.1
-export PATH=$GOROOT/bin:$PATH
-
 tinygo build -target wasm -tags "purego noasm" -o node/test/testapp.wasm ./example/testapp
